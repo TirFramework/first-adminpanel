23dd77ffb85cc6d08f0c9e7e47a80796ea8c9fe6
mehrdad <javadabbasi72@gmail.com>
2020-04-10 18:49:37 +0430

    remove md

---
+++ src/Controllers/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tir\FirstPanel\Controllers;
+
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        return view('Pages.dashboard');
+    }
+}
